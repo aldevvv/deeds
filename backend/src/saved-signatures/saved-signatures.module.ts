5c49bb7d41a5ec02cc1fcc50b7c81de9c3b67dd9
JackMatthew112233 <rejekkimanalu22@gmail.com>
2025-11-13 14:20:52 +0800

    Updates 13 Nov 2025

--- backend/src/saved-signatures/saved-signatures.module.ts
+++ backend/src/saved-signatures/saved-signatures.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { SavedSignaturesController } from './saved-signatures.controller';
 import { SavedSignaturesService } from './saved-signatures.service';
 import { PrismaModule } from '../prisma/prisma.module';
+import { SupabaseModule } from '../common/supabase/supabase.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, SupabaseModule],
   controllers: [SavedSignaturesController],
   providers: [SavedSignaturesService],
   exports: [SavedSignaturesService],
